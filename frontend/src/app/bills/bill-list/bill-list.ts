3bc088e42b6ce5fade49039531e8f1ecc0947fa3
Joshua Pelton-Stroud <jpstroud@gmail.com>
2025-12-17 15:39:05 -0500

    list implementations

--- frontend/src/app/bills/bill-list/bill-list.ts
+++ frontend/src/app/bills/bill-list/bill-list.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, input, computed, viewChild, effect } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { BILLS } from '../bills';
 
 @Component({
   selector: 'app-bill-list',
-  imports: [],
+  imports: [MatTableModule, MatSortModule, MatSort],
   templateUrl: './bill-list.html',
   styleUrl: './bill-list.sass',
 })
 export class BillList {
+  readonly displayedColumns: string[] = ['id', 'title', 'activeVersion', 'publishedDateTime'];
+  sort = viewChild(MatSort);
+  dataSource = new MatTableDataSource(BILLS);
 
+  constructor() {
+    effect(() => {
+      const sortDirective = this.sort();
+      if (sortDirective) {
+        this.dataSource.sort = sortDirective;
+      }
+    });
+  }
 }
--- frontend/src/app/legislators/legislator-list/legislator-list.ts
+++ frontend/src/app/legislators/legislator-list/legislator-list.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, input, computed, viewChild, effect } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { ASSEMBLY_MEMBERS, SENATORS } from '../legislators';
 
 @Component({
   selector: 'app-legislator-list',
-  imports: [],
+  imports: [MatTableModule, MatSortModule, MatSort],
   templateUrl: './legislator-list.html',
   styleUrl: './legislator-list.sass',
 })
 export class LegislatorList {
+  readonly displayedColumns: string[] = ['lname', 'fname', 'party', 'district'];
+  chamber = input.required<string>();
+  sort = viewChild(MatSort);
+  dataSource = computed(() => {
+    if (this.chamber() === 'assembly') {
+      return new MatTableDataSource(ASSEMBLY_MEMBERS);
+    } else {
+      return new MatTableDataSource(SENATORS);
+    }
+  });
 
+  constructor() {
+    effect(() => {
+      const sortDirective = this.sort();
+      if (sortDirective) {
+        this.dataSource().sort = sortDirective;
+      }
+    });
+  }
 }
