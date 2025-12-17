d1c963a140f2952c41586420f25a9c553f307eb7
Joshua Pelton-Stroud <jpstroud@gmail.com>
2025-12-17 12:38:34 -0500

    init

---
+++ frontend/src/app/myreps/myreps.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myreps',
+  imports: [],
+  templateUrl: './myreps.html',
+  styleUrl: './myreps.sass',
+})
+export class MyReps {
+
+}
