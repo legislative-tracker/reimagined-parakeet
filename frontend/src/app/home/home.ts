e0bce4247af00691cdd8a888e0ffd8b4acfce728
Joshua Pelton-Stroud <jpstroud@gmail.com>
2025-12-17 13:12:49 -0500

    added components

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BillList } from "../bills/bill-list/bill-list";
+import { LegislatorList } from "../legislators/legislator-list/legislator-list";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatTabsModule, BillList, LegislatorList],
   templateUrl: './home.html',
   styleUrl: './home.sass',
 })
-export class Home {
-
-}
+export class Home {}
