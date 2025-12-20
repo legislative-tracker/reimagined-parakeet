4f56f26b0cca12126a741fd5f8d61478b3650a15
Joshua Pelton-Stroud <jpstroud@gmail.com>
2025-12-20 15:06:11 -0500

    update temp data to match prod data type

--- frontend/src/app/legislators/legislator-list/legislator-list.ts
+++ frontend/src/app/legislators/legislator-list/legislator-list.ts
@@ -1,7 +1,7 @@
 import { Component, input, computed, viewChild, effect } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatSortModule, MatSort } from '@angular/material/sort';
-import { ASSEMBLY_MEMBERS, SENATORS } from '../legislators';
+import { MEMBERS, Legislator } from '../legislators';
 
 @Component({
   selector: 'app-legislator-list',
@@ -10,14 +10,18 @@ import { ASSEMBLY_MEMBERS, SENATORS } from '../legislators';
   styleUrl: './legislator-list.sass',
 })
 export class LegislatorList {
-  readonly displayedColumns: string[] = ['lname', 'fname', 'party', 'district'];
+  readonly displayedColumns: string[] = ['lname', 'fname', 'district', 'party'];
   chamber = input.required<string>();
   sort = viewChild(MatSort);
   dataSource = computed(() => {
-    if (this.chamber() === 'assembly') {
-      return new MatTableDataSource(ASSEMBLY_MEMBERS);
+    if (this.chamber() === 'ASSEMBLY') {
+      return new MatTableDataSource(
+        MEMBERS.filter((m) => m.memberships.some((ms) => ms.organization_id === 'ASSEMBLY'))
+      );
     } else {
-      return new MatTableDataSource(SENATORS);
+      return new MatTableDataSource(
+        MEMBERS.filter((m) => m.memberships.some((ms) => ms.organization_id === 'SENATE'))
+      );
     }
   });
 
@@ -28,5 +32,14 @@ export class LegislatorList {
         this.dataSource().sort = sortDirective;
       }
     });
+  }
+
+  getDistrict(legislator: Legislator): string {
+    const chamberMembership = legislator.memberships.find((ms) => ms.label === 'chamber');
+    return chamberMembership ? chamberMembership.area_id : '';
+  }
+  getParty(legislator: Legislator): string {
+    const partyMembership = legislator.memberships.find((ms) => ms.label === 'party');
+    return partyMembership ? partyMembership.organization_id : '';
   }
 }
--- frontend/src/app/legislators/legislators.ts
+++ frontend/src/app/legislators/legislators.ts
@@ -1,30 +1,216 @@
-export interface LegislatorShort {
-  fname: string;
-  lname: string;
-  district: number;
-  party: string;
+import { Person } from 'popolo-types';
+
+export interface Legislator extends Person {
+  memberships: {
+    organization_id: string;
+    label: string;
+    area_id: string;
+  }[];
+  sponsorships?: {
+    billId: string;
+    version: string;
+    name: string;
+  }[];
 }
 
-export const ASSEMBLY_MEMBERS: LegislatorShort[] = [
-  { fname: 'Jane', lname: 'Doe', district: 1, party: 'Democrat' },
-  { fname: 'John', lname: 'Doe', district: 2, party: 'Republican' },
-  { fname: 'Jack', lname: 'Horner', district: 3, party: 'Democrat' },
-  { fname: 'Jill', lname: 'Tate', district: 4, party: 'Republican' },
-  { fname: 'Joe', lname: 'Guy', district: 5, party: 'Democrat' },
-  { fname: 'Jim', lname: 'Manly', district: 6, party: 'Republican' },
-  { fname: 'Jerry', lname: 'Falwell', district: 7, party: 'Democrat' },
-  { fname: 'Jeff', lname: 'Comey', district: 8, party: 'Independant' },
-  { fname: 'Jenny', lname: 'Ortega', district: 9, party: 'Democrat' },
-];
+export const MEMBERS: Legislator[] = [
+  {
+    given_name: 'Jane',
+    family_name: 'Doe',
+    memberships: [
+      {
+        organization_id: 'ASSEMBLY',
+        label: 'chamber',
+        area_id: '1',
+      },
+    ],
+  },
+  {
+    given_name: 'John',
+    family_name: 'Doe',
+    memberships: [
+      {
+        organization_id: 'ASSEMBLY',
+        label: 'chamber',
+        area_id: '2',
+      },
+    ],
+  },
+  {
+    given_name: 'Jack',
+    family_name: 'Horner',
+    memberships: [
+      {
+        organization_id: 'ASSEMBLY',
+        label: 'chamber',
+        area_id: '3',
+      },
+    ],
+  },
+  {
+    given_name: 'Jill',
+    family_name: 'Tate',
+    memberships: [
+      {
+        organization_id: 'ASSEMBLY',
+        label: 'chamber',
+        area_id: '4',
+      },
+    ],
+  },
+  {
+    given_name: 'Joe',
+    family_name: 'Guy',
+    memberships: [
+      {
+        organization_id: 'ASSEMBLY',
+        label: 'chamber',
+        area_id: '5',
+      },
+    ],
+  },
+  {
+    given_name: 'Jim',
+    family_name: 'Manly',
+    memberships: [
+      {
+        organization_id: 'ASSEMBLY',
+        label: 'chamber',
+        area_id: '6',
+      },
+    ],
+  },
+  {
+    given_name: 'Jerry',
+    family_name: 'Falwell',
+    memberships: [
+      {
+        organization_id: 'ASSEMBLY',
+        label: 'chamber',
+        area_id: '7',
+      },
+    ],
+  },
+  {
+    given_name: 'Jeff',
+    family_name: 'Comey',
+    memberships: [
+      {
+        organization_id: 'ASSEMBLY',
+        label: 'chamber',
+        area_id: '8',
+      },
+    ],
+  },
+  {
+    given_name: 'Jenny',
+    family_name: 'Ortega',
+    memberships: [
+      {
+        organization_id: 'ASSEMBLY',
+        label: 'chamber',
+        area_id: '9',
+      },
+    ],
+  },
 
-export const SENATORS: LegislatorShort[] = [
-  { fname: 'Jimminy', lname: 'Cricket', district: 1, party: 'Democrat' },
-  { fname: 'John', lname: 'Mulaney', district: 2, party: 'Democrat' },
-  { fname: 'Jack', lname: 'Reacher', district: 3, party: 'Democrat' },
-  { fname: 'Jill', lname: 'Stein', district: 4, party: 'Green' },
-  { fname: 'Joe', lname: 'Montagna', district: 5, party: 'Democrat' },
-  { fname: 'Jim', lname: 'Fix', district: 6, party: 'Republican' },
-  { fname: 'Jerry', lname: 'Seinfeld', district: 7, party: 'Democrat' },
-  { fname: 'Jeff', lname: 'Block', district: 8, party: 'Independant' },
-  { fname: 'Jenny', lname: 'Lane', district: 9, party: 'Democrat' },
+  {
+    given_name: 'Jimminy',
+    family_name: 'Cricket',
+    memberships: [
+      {
+        organization_id: 'SENATE',
+        label: 'chamber',
+        area_id: '1',
+      },
+    ],
+  },
+  {
+    given_name: 'John',
+    family_name: 'Mulaney',
+    memberships: [
+      {
+        organization_id: 'SENATE',
+        label: 'chamber',
+        area_id: '1',
+      },
+    ],
+  },
+  {
+    given_name: 'Jack',
+    family_name: 'Reacher',
+    memberships: [
+      {
+        organization_id: 'SENATE',
+        label: 'chamber',
+        area_id: '1',
+      },
+    ],
+  },
+  {
+    given_name: 'Jill',
+    family_name: 'Stein',
+    memberships: [
+      {
+        organization_id: 'SENATE',
+        label: 'chamber',
+        area_id: '1',
+      },
+    ],
+  },
+  {
+    given_name: 'Joe',
+    family_name: 'Montagna',
+    memberships: [
+      {
+        organization_id: 'SENATE',
+        label: 'chamber',
+        area_id: '1',
+      },
+    ],
+  },
+  {
+    given_name: 'Jim',
+    family_name: 'Fix',
+    memberships: [
+      {
+        organization_id: 'SENATE',
+        label: 'chamber',
+        area_id: '2',
+      },
+    ],
+  },
+  {
+    given_name: 'Jerry',
+    family_name: 'Seinfeld',
+    memberships: [
+      {
+        organization_id: 'SENATE',
+        label: 'chamber',
+        area_id: '3',
+      },
+    ],
+  },
+  {
+    given_name: 'Jeff',
+    family_name: 'Block',
+    memberships: [
+      {
+        organization_id: 'SENATE',
+        label: 'chamber',
+        area_id: '4',
+      },
+    ],
+  },
+  {
+    given_name: 'Jenny',
+    family_name: 'Lane',
+    memberships: [
+      {
+        organization_id: 'SENATE',
+        label: 'chamber',
+        area_id: '5',
+      },
+    ],
+  },
 ];
