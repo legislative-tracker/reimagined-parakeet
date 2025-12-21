efbb3fa8864ee308963f7b52b6514e8db4e55cdc
Joshua Pelton-Stroud <jpstroud@gmail.com>
2025-12-21 14:40:41 -0500

    added routing logic for additional states in the future

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', loadComponent: () => import('./home/home').then((m) => m.Home) },
+  { path: '', pathMatch: 'full', redirectTo: 'ny' },
+  {
+    path: ':state',
+    pathMatch: 'full',
+    loadComponent: () => import('./home/home').then((m) => m.Home),
+  },
   { path: 'about', loadComponent: () => import('./about/about').then((m) => m.About) },
   { path: 'profile', loadComponent: () => import('./profile/profile').then((m) => m.Profile) },
   { path: 'myreps', loadComponent: () => import('./myreps/myreps').then((m) => m.MyReps) },
