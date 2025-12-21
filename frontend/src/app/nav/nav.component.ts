061b40bbdf38cad5f4839e591542a90dacefc802
Joshua Pelton-Stroud <jpstroud@gmail.com>
2025-12-21 14:41:19 -0500

    added (disabled) state selector

--- frontend/src/app/nav/nav.component.ts
+++ frontend/src/app/nav/nav.component.ts
@@ -1,6 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -8,7 +11,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { Router, RouterOutlet, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -20,17 +23,26 @@ import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
     MatSidenavModule,
     MatListModule,
     MatIconModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
     AsyncPipe,
     RouterOutlet,
-    RouterLinkWithHref
-]
+    RouterLinkWithHref,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavComponent {
+  private router = inject(Router);
   private breakpointObserver = inject(BreakpointObserver);
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map((result) => result.matches),
+    shareReplay()
+  );
+  stateControl = new FormControl({ value: 'ny', disabled: true });
+
+  onSelectionChange(state: string) {
+    this.router.navigate([state]);
+  }
 }
