c8401bd5fcaf833140c26e4e2a4be2b056a7eccf
Joshua Pelton-Stroud <jpstroud@gmail.com>
2025-12-23 19:02:45 -0500

    track env

--- frontend/src/env/dev.ts
+++ frontend/src/env/dev.ts
@@ -1,3 +1,14 @@
 export const env = {
-  firebase: {},
+  firebase: {
+    projectId: 'tracker-cwapolitical-org',
+    appId: '1:204649723919:web:090e371e56888f2ad94b7a',
+    databaseURL: 'https://tracker-cwapolitical-org.firebaseio.com',
+    storageBucket: 'tracker-cwapolitical-org.firebasestorage.app',
+    apiKey: 'AIzaSyDpF6H0fdnLHNdwUlvBJPyBJqcKR0a1JEc',
+    authDomain: 'tracker-cwapolitical-org.firebaseapp.com',
+    messagingSenderId: '204649723919',
+    measurementId: 'G-GLL6TYD8QY',
+    projectNumber: '204649723919',
+    version: '2',
+  },
 };
