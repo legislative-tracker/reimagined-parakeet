38d2bfb7a03e37b9e07daa03b8d5af58339233da
Joshua Pelton-Stroud <jpstroud@gmail.com>
2025-12-29 17:49:20 -0500

    updated type safety

--- frontend/src/app/models/user.ts
+++ frontend/src/app/models/user.ts
@@ -1,6 +1,22 @@
+import { Legislator } from './legislature';
+
 export interface AppUser {
   displayName: string;
   email: string;
   uid: string;
   lastLogin: string;
+  phoneNumber?: string;
+  photoURL: string;
+  districts: {
+    federal: string;
+    state: {
+      assembly: string;
+      senate: string;
+    };
+  };
+  legislators: {
+    federal: Legislator[];
+    state: Legislator[];
+  };
+  favorites?: string[];
 }
