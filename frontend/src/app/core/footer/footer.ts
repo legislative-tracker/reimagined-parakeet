f4e9dc5edcc196bb863d236a20b745236c7b1426
Joshua Pelton-Stroud <jpstroud@gmail.com>
2026-01-04 15:12:59 -0500

    feat(footer): added info & links, changed background to primary

--- frontend/src/app/core/footer/footer.ts
+++ frontend/src/app/core/footer/footer.ts
@@ -1,14 +1,17 @@
 import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import pkg from '../../../../package.json';
+import { buildDate } from '../../build-info';
 
 @Component({
   selector: 'app-footer',
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
   currentYear = signal(new Date().getFullYear());
   appVersion = signal(pkg.version);
+  buildTimestamp = signal(buildDate);
 }
