0754050aafa6802bd982e802f2e137417919b43d
Joshua Pelton-Stroud <jpstroud@gmail.com>
2026-01-06 11:53:24 -0500

    refactor(table): removed testClick and replaced @Output() with output<T>()

--- frontend/src/app/shared/table/table.component.ts
+++ frontend/src/app/shared/table/table.component.ts
@@ -1,8 +1,9 @@
-import { Component, input, computed } from '@angular/core';
-import { MatTableModule, MatTable } from '@angular/material/table';
-import { MatSortModule, MatSort } from '@angular/material/sort';
+import { Component, input, computed, output } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 import { RouterLink } from '@angular/router';
 
+// App imports
 import { ColumnConfig } from '@app-models/column-config';
 
 @Component({
@@ -14,16 +15,15 @@ import { ColumnConfig } from '@app-models/column-config';
 export class TableComponent<T> {
   dataSource = input.required<T[]>();
   columnSource = input.required<ColumnConfig<T>[]>();
+  stateCd = input.required<string>(); // Passed from parent or route
   routeType = input.required<'bill' | 'member'>();
   chamber = input<'SENATE' | 'ASSEMBLY'>();
-  stateCd = input<string>(); // Passed from parent or route
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = computed(() => this.columnSource().map((c) => c.key));
 
-  testClick(row: any) {
-    console.log('Row clicked!', row);
-    console.log('Target State:', this.stateCd());
-    console.log('Target ID:', row.id);
+  rowClick = output<T>();
+
+  onRowClick(row: T) {
+    this.rowClick.emit(row);
   }
 }
