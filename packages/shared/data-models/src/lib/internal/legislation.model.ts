e352a60557e1136b796d454371dac26e82dbdce7
Joshua Pelton-Stroud <jpstroud@gmail.com>
2026-01-21 11:00:10 -0500

    updt model

--- packages/shared/data-models/src/lib/internal/legislation.model.ts
+++ packages/shared/data-models/src/lib/internal/legislation.model.ts
@@ -2,4 +2,10 @@ import type { Bill } from '../open-states/bill.model';
 
 export interface Legislation extends Bill {
   current_version: string;
+  cosponsorships: {
+    [key: string]: {
+      id: string;
+      name: string;
+    }[];
+  };
 }
