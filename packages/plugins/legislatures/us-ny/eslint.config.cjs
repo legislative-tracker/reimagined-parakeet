e7fe96b8dc3df52f332b1d40299d752b60d96403
Joshua Pelton-Stroud <jpstroud@gmail.com>
2026-01-21 11:31:41 -0500

    linting fix

--- packages/plugins/legislatures/us-ny/eslint.config.cjs
+++ packages/plugins/legislatures/us-ny/eslint.config.cjs
@@ -3,9 +3,10 @@ const baseConfig = require('../../../../eslint.config.js');
 module.exports = [
   ...baseConfig,
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ['**/*.ts'],
     languageOptions: {
       parserOptions: {
+        parser: require('jsonc-eslint-parser'),
         // Point to the specific configs that include your library and test files
         project: ['./tsconfig.lib.json', './tsconfig.spec.json'],
         tsconfigRootDir: __dirname,
