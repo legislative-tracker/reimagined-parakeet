8a35853914b14aac3cb045897a35fe72594728fa
Joshua Pelton-Stroud <jpstroud@gmail.com>
2026-01-22 17:07:24 -0500

    updt

--- packages/server/triggers/src/main.ts
+++ packages/server/triggers/src/main.ts
@@ -1,24 +1,6 @@
 // import * as logger from 'firebase-functions/logger';
 import { onRequest } from 'firebase-functions/v2/https';
 
-import { backendConfig } from '@legislative-tracker/shared-config-secrets';
-// import { mergeStateAndOSPeopleData } from '@legislative-tracker/plugin-shared-utils';
-// import { fetchLegislators } from '@legislative-tracker/plugin-openstates';
-import { fetchBills } from '@legislative-tracker/plugin-leg-us-ny';
-import { updateFirestoreLegislation } from '@legislative-tracker/server-data-access';
-import type { Legislation } from '@legislative-tracker/shared-data-models';
-
-export const helloWorld = onRequest(
-  {
-    secrets: backendConfig.allSecrets,
-  },
-  async (request, response) => {
-    const stateCd = 'us-ny';
-
-    const bills = await fetchBills(['S4465-2025'], backendConfig.usNyApiKey);
-
-    await updateFirestoreLegislation(stateCd, bills as Legislation[]);
-
-    response.send(bills);
-  },
-);
+export const helloWorld = onRequest(async (request, response) => {
+  response.send(`Updates completed at ${new Date().toISOString()}`);
+});
