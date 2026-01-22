447d53dcc4d67aaa0f809d00b2e5d269c3b55d21
Joshua Pelton-Stroud <jpstroud@gmail.com>
2026-01-22 18:54:20 -0500

    renamed fn for clarity

--- packages/server/data-access/src/lib/bill-sync.service.ts
+++ packages/server/data-access/src/lib/bill-sync.service.ts
@@ -1,11 +1,11 @@
 import { logger } from 'firebase-functions/v2';
 import type { Legislation } from '@legislative-tracker/shared-data-models';
+import { PluginFactory } from './plugin-factory.service.js';
 
 import {
   db,
   getTypedCollection,
 } from '@legislative-tracker/shared-config-firebase';
-import { PluginFactory } from './plugin-factory.service.js';
 
 /**
  * @description Service responsible for coordinating the sync process between plugins and Firestore.
@@ -22,7 +22,7 @@ export class BillSyncService {
       try {
         logger.info(`Starting sync for state: ${plugin.stateCode}`);
 
-        const billList = await this._getBills(plugin.stateCode);
+        const billList = await this._getBillIds(plugin.stateCode);
         const bills = await plugin.fetchBills(billList);
         await this._updateFirestore(plugin.stateCode, bills);
 
@@ -47,7 +47,7 @@ export class BillSyncService {
     const bulkWriter = db.bulkWriter();
 
     bills.forEach((bill) => {
-      const path = `/legislatures/${stateCode}/legislation`;
+      const path = `legislatures/${stateCode}/legislation`;
 
       // Use stateCode and bill ID to create a unique document path
       const docRef = db.collection(path).doc(bill.id);
@@ -57,7 +57,7 @@ export class BillSyncService {
     await bulkWriter.close();
   }
 
-  private async _getBills(stateCode: string): Promise<string[]> {
+  private async _getBillIds(stateCode: string): Promise<string[]> {
     const path = `legislatures/${stateCode}/legislation`;
     const col = await getTypedCollection<Legislation>(path).get();
 
